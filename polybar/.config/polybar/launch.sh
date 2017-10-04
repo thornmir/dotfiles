b7bd6be4c9ce5a38b65f2fba52dc7d24ce9e6f61
Florian Postic <thornmir@gmail.com>
2017-10-04 17:57:52 +0200

    Minor fixes in polybar conf and update of gtk2.0 theme

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,7 +5,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 for m in "$(xrandr -q | grep " connected" | cut -d " " -f1)"; do
     if [[ "$m" == "$(xrandr -q |grep "+0+0" |cut -d " " -f1)" ]];then
-        MONITOR="$m" polybar primary &
+        MONITOR="$m" polybar primary --log=info &
     elif "$m" |grep -q "VGA" ; then
         MONITOR="$m" polybar presentation &:
     else
