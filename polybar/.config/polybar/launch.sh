d5495206f7687f4621dd154ed6a2b7a8362ec4db
Florian Postic <thornmir@gmail.com>
2017-10-04 11:35:13 +0200

    Polybar is now portable

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
-polybar primary &
-if xrandr -q | grep -c "\*" | grep -q "2"
-then
-  if xrandr -q | grep "VGA1 connected"
-  then
-    polybar presentation &
-  else
-    polybar secondary &
-  fi
-fi
+
+for m in "$(xrandr -q | grep " connected" | cut -d " " -f1)"; do
+    if [[ "$m" == "$(xrandr -q |grep "+0+0" |cut -d " " -f1)" ]];then
+        MONITOR="$m" polybar primary &
+    elif "$m" |grep -q "VGA" ; then
+        MONITOR="$m" polybar presentation &:
+    else
+        MONITOR="$m" polybar secondary &:
+    fi        
+done 
